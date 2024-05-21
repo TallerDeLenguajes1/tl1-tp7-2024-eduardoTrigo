3973858279490adf5cb075b675eeb01c8d3115d8
Plox179 <leandroleon799@gmail.com>
2024-05-21 12:58:57 -0300

    second commit

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+class Calculadora
+{
+    console.Read();
+    int dato1;
+    int dato2;
+
+    void Sumar(double termino)
+    {
+        int aux;
+        dato1 + dato2=aux;
+        console.WriteLine(aux)
+    }
+    
+    void Restar(double termino)
+    {
+        int aux;
+         
+        if (dato1 > dato2)
+        {
+            dato1 - dato2 = aux;
+        }
+        else
+        {
+            dato2 - dato1 = aux;
+        }
+        console.WriteLine(aux)
+    }
+
+}
